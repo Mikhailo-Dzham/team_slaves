4e8147e5ddcafa08e220c84428e40702975cb45d
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-02-06 00:11:59 +0200

    added a call to is_prime() in main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from utils import factorial
 if __name__ == "__main__":
     n = int(input("Введіть число: "))
     print(f"Факторіал {n} = {factorial(n)}\n")
+
+from utils import is_prime
+if __name__ == "__main__":
+    n = int(input("Введіть n: "))
+    print(f"{n} {"не " * (not(is_prime(n)))}є простим числом")
+
+
+
