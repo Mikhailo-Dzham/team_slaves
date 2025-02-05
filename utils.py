c40fe66ddb96a44e332cee8ee752e3fe867471e3
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-02-06 00:01:13 +0200

    added is_prime() function to utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ def factorial(n):
     for i in range(1, n+1):
         res *= i
     return res
+
+
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
